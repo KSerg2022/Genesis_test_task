2aff50b07983b5466aa89b60692a9208eab05139
KSerg2022 <koshevoys@gmail.com>
2023-05-26 16:15:05 +0200

    changed settings.py

--- settings.py
+++ settings.py
@@ -3,7 +3,6 @@ import datetime
 from pathlib import Path
 
 base_dir = Path(__file__).parent
-time_stamp = datetime.datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
 
 data_dir = base_dir / 'app' / 'data'
 
